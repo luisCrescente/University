5955613eece444c154f13af5c92ce0155814c23b
user <user@DESKTOP-L0SPK6C>
2022-11-20 14:17:08 -0300

    Agregar archivos de proyecto.

---
+++ UniversityApiBackend/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApiBackend.Models.DataModels;
+
+namespace UniversityApiBackend.DataAccess
+{
+    public class UniversityDBContext: DbContext
+    {
+        public UniversityDBContext(DbContextOptions <UniversityDBContext> options): base(options)
+        {
+
+        }
+        //TODO: Add DbStets (Tables of our Date Base)
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Curso>? Cursos { get; set; }
+    }
+}
